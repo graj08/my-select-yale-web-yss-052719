d79f2a827ad0b7f558dfc00caf99c4635f83ff77
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-14 02:01:38 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,7 +4,7 @@ def my_select(collection)
  return_array = Array.new
  while i<collection.length do
    new_value = yield collection[i]
-   if new_value == true 
+   if new_value == true
      return_array << collection[i]
    end
    i +=1
